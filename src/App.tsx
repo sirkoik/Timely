0178ce59c7c1f85b77f3da371e273dcd846a509f
Alexander Koik-Cestone <akoikcestone@gmail.com>
2022-04-17 09:38:23 -0400

    added material UI styles and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { CssBaseline } from '@mui/material'; // Material UI CSS baseline for better compatibility
+import Button from '@mui/material/Button';
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
@@ -6,19 +8,22 @@ import timers from './shared/timers';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {timers.map((timer, index) => (
-          <Timer
-            name={timer.name}
-            t1={timer.date}
-            key={index}
-            config={timer.config}
-          />
-        ))}
-      </header>
-      <main></main>
-    </div>
+    <>
+      <CssBaseline />
+      <div className="App">
+        <header className="App-header">
+          {timers.map((timer, index) => (
+            <Timer
+              name={timer.name}
+              t1={timer.date}
+              key={index}
+              config={timer.config}
+            />
+          ))}
+        </header>
+        <main></main>
+      </div>
+    </>
   );
 }
 
