7c01c7712b42e1aad402f376aabe0cfc5d70e900
Alexander Koik-Cestone <akoikcestone@gmail.com>
2022-04-17 19:21:37 -0400

    add Material UI icons, header

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-import { CssBaseline } from '@mui/material'; // Material UI CSS baseline for better compatibility
-import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline'; // Material UI CSS baseline for better compatibility
+
 import React from 'react';
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Timer from './components/Timer';
+import Header from './components/UI/Header';
 import timers from './shared/timers';
 
 function App() {
   return (
     <>
       <CssBaseline />
-      <div className="App">
-        <header className="App-header">
-          {timers.map((timer, index) => (
-            <Timer
-              name={timer.name}
-              t1={timer.date}
-              key={index}
-              config={timer.config}
-            />
-          ))}
-        </header>
-        <main></main>
-      </div>
+      <header className="App-header">
+        <Header />
+      </header>
+      <main>
+        {timers.map((timer, index) => (
+          <Timer
+            name={timer.name}
+            t1={timer.date}
+            key={index}
+            config={timer.config}
+          />
+        ))}
+      </main>
     </>
   );
 }
